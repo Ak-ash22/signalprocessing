bfcd17a0580ba15e7f5046b4b3845a197b9f6b39
Akash M <97802146+Ak-ash22@users.noreply.github.com>
2023-11-12 09:58:03 +0100

    Update timbre.py

--- Numerics/timbre.py
+++ Numerics/timbre.py
@@ -1,8 +1,1 @@
-import numpy as np
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-import soundfile as sf
-import sounddevice as sd
 
-filepath = r'Users\14aka\OneDrive\Documents\Octave\Numerics\TDSound\PianoLaPeriod.wav'
-data, samplerate = sf.read(filepath)
